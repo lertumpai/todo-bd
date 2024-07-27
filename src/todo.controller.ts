eaf4e105207fa196fbdcdda5e48c2997e8ace4f7
sorawit <sorawit@senestia.com>
2024-07-27 22:32:20 +0700

    implement controller

--- src/todo.controller.ts
+++ src/todo.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { TodoService } from "./todo.service";
+import { TodoDTO } from "./todo.dto";
 
-@Controller()
+@Controller("v1/todos")
 export class TodoController {
-  constructor(private readonly appService: TodoService) {}
+  constructor(private readonly todoService: TodoService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  get() {
+    console.log("123");
+    return this.todoService.getAll();
+  }
+
+  @Post()
+  create(@Body() body: TodoDTO) {
+    return this.todoService.create({
+      title: body.title,
+      note: body.note,
+    });
   }
 }
---
+++ src/todo.dto.ts
@@ -0,0 +1,4 @@
+export type TodoDTO = {
+  title: string;
+  note: string;
+};
